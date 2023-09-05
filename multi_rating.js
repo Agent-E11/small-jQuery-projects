2948953027146a73a120e2a175561e0953e911e2
Eamon <eamon.r.burns@gmail.com>
2023-09-05 13:52:12 -0700

    - Improved `multi_rating` - Now colors stars based on their index in the `ul` instead of a class Renamed variables for clarity

--- multi_rating.js
+++ multi_rating.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     // Populate each rating box with stars
-    $('.rating-list').each(function(index, box){
-        $(box).html(`
-            <a class="list-inline-item m-0 star star0">&#9733;</a>
-            <a class="list-inline-item m-0 star star1">&#9733;</a>
-            <a class="list-inline-item m-0 star star2">&#9733;</a>
-            <a class="list-inline-item m-0 star star3">&#9733;</a>
-            <a class="list-inline-item m-0 star star4">&#9733;</a>
+    $('.rating-list').each(function(_, rating_list){
+        $(rating_list).html(`
+            <a class="list-inline-item m-0 star">&#9733;</a>
+            <a class="list-inline-item m-0 star">&#9733;</a>
+            <a class="list-inline-item m-0 star">&#9733;</a>
+            <a class="list-inline-item m-0 star">&#9733;</a>
+            <a class="list-inline-item m-0 star">&#9733;</a>
         `).addClass('list-inline');
     });
 
@@ -14,36 +14,26 @@ $(document).ready(function(){
     $('.star').click(function(event){
         event.preventDefault(); // Don't redirect
 
-        const star = $(this);
-        const rating_list = $(star).parent();
+        const clicked_star = $(this);
+        const rating_list = $(clicked_star).parent();
 
         // The index of the current star, in relation to the list of stars
         // (Ignores non-stars)
-        let index = $(`#${rating_list.attr('id')} a.star`).index(star)
-        // An array of 
-        let class_array = [...Array(index+1).keys()].map(function(value){
-            return `star${value}`
-        });
+        let index = $(`#${rating_list.attr('id')} a.star`).index(clicked_star);
 
         // Color the stars based on index
         $(`#${rating_list.attr('id')} a.star`).each(function(_, obj){
-            var active = false;
-            const star = $(this)
+            const current_star = $(this);
 
-            // If the star has any of the classes, set `active` to true
-            for (let star_class of class_array) {
-                if (star.hasClass(star_class)) {
-                    active = true;
-                }
-            }
-
-            // Give the star the appropriate class based on whether it is active or not
-            if (active) {
-                star.addClass('active-star');
-                star.removeClass('inactive-star');
+            // If "the index of the current star, in the list of stars" is less than "the index of the clicked star"
+            if ($(`#${rating_list.attr('id')} a.star`).index(current_star) <= index) {
+                // Give it the active color
+                current_star.addClass('active-star');
+                current_star.removeClass('inactive-star');
             } else {
-                star.addClass('inactive-star');
-                star.removeClass('active-star');
+                // Give it the inactive color
+                current_star.addClass('inactive-star');
+                current_star.removeClass('active-star');
             }
         });
     });
